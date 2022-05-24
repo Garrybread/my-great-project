228d6718ee33b57076466f236ca840fe5b468df4
Garrybread <ryanstevencooper@gmail.com>
2022-05-24 19:16:13 +0000

    Nested Resources

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
-end
+    has_many :reviews, dependent: :destroy
+    validates :title, presence: true, uniqueness: true
+    validates :description, presence: true
+    validates :price, presence: true
+  end
--- db/migrate/20220523185756_create_books.rb
+++ db/migrate/20220523185756_create_books.rb
@@ -1,10 +1,10 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :description
       t.float :price
-
+    
       t.timestamps
     end
   end

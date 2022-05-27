5f1b1197c04f217ec7a0ff914ae663db57d9de3a
Garrybread <ryanstevencooper@gmail.com>
2022-05-27 17:39:25 +0000

    libraries are done

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,5 +1,5 @@
 class LibrariesController < ApplicationController
-    
+    before_action :set_library, only: [:show, :edit, :update, :destroy]
 
     def index
         @libraries = Library.all
@@ -20,16 +20,34 @@ class LibrariesController < ApplicationController
     end
 
     def show
-        @library = Library.find(params[:id])
     end
 
     def edit
     end
 
+    def update
+      if @library.update(library_params)
+        redirect_to library_url(@library), notice: "Library was successfully updated."
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+
+    def destroy
+      @library.destroy
+      redirect_to libraries_url, notice: "Library was successfully deleted."
+    end
+
+
     private
 
-        def library_params
-            params.require(:library).permit(:name, :address, :number)
-        end
+      def set_library
+        @library = Library.find(params[:id])
+      end
+
+      def library_params
+        params.require(:library).permit(:name, :address, :number, book_ids: [])
+      end
 
 end

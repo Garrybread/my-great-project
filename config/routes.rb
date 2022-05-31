75975872417e28ed661b219a041338deb0bf5b76
Garrybread <ryanstevencooper@gmail.com>
2022-05-31 14:29:46 +0000

    bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :home, only: :index
   resources :libraries
   resources :books do
     resources :reviews
   end
 
-  root 'libraries#welcome'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
